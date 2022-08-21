24f5fc23233da3d095ffc2dbaecb4bb0faaee5c9
Jim Nguyen <95522881+jimbn@users.noreply.github.com>
2022-08-20 20:30:25 -0700

    comment fetch options

---
+++ client/src/__tests__/dungeonGen.test.js
@@ -0,0 +1,13 @@
+// level 1 - 5 map
+    // increase in difficulty = bigger map and more difficult monster
+    // each level is a multiple of 5 grid. (5x5, 10x10, 15x15, 20x20, 25x25)
+    // level 1
+        //              exit
+        //     |_|_|_|_|_|
+        //     |_|_|_|_|_|
+        //     |_|_|_|_|_|
+        //     |_|_|_|_|_|
+        //     | | | | | |      
+        //   enter
+
+
--- client/src/__tests__/fetchApi.test.js
+++ client/src/__tests__/fetchApi.test.js
@@ -12,3 +12,41 @@ test('fetch API', () => {
 
 
 testHello.append(document.getElementById(`testCode`));
+
+// CHARACTER
+// == Classes  (http://www.dnd5eapi.co/api/classes/{index})
+  
+
+// == Ability Score/Stats (http://www.dnd5eapi.co/api/ability-scores/{index})
+    // Charisma, Consititution, Dexterity, Intelligence, Strength, Wisdom
+    // Will be use to determine health/damage output/ resistence
+
+// == spells/skills (http://www.dnd5eapi.co/api/classes/{index}/spells)
+    // Pathing for skills
+        // Character Creation > Class > when class choose, user profile will generate potential spells
+        // i.e. backend: user choose wizard. player profile save http://www.dnd5eapi.co/api/classes/11/spells in provile
+            // only those in that api will be accessible to the user
+
+// Monsters
+// == Monster (http://www.dnd5eapi.co/api/monsters/{index})
+
+// Battle
+// == Damage Types (http://www.dnd5eapi.co/api/damage-types/{index})
+  // 1) Roll initiative (dice roll + Dex Modfier)
+  // >> Turn based
+  // *****
+    // 1) Declare an Attack
+    // 2) Determine bonuses & penalties (Advantage or Disadvantage)
+    // 3) Attack Chance = roll + Attack Modifier
+    // 4) Attack Chance vs Armor Check
+        // If Attack Chance >= Armor Check = hit
+        // Total Dmg = Damage die + Strength/Dex modifier (Spells just do dmg, no mod)
+        // Remaining Health - Total Dmg = RemaingHealth
+            // If RemaingHealth <= 0 = dies
+  // >> Atttack Chance: Dice roll ---> Hit Chance has to be greater or equal to dodge
+  // >> (Reciver's Armor + Health) - (Dealing Weapon dmg + Stats) = Reciever's remaining Health
+
+// Items/Equipments
+  // == Equipment/Item (http://www.dnd5eapi.co/api/equipment-categories/{index})
+    // Use categories to filter items. Will have to do multiple fetch per items.
+    // i.e. Fetch catgory (index) > fetch object based on the index within that category
