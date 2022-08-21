782ec55eeb0ee939dbb8ede6eb5d29653998301d
Jim Nguyen <95522881+jimbn@users.noreply.github.com>
2022-08-20 17:56:28 -0700

    add html/js fetchApi

---
+++ client/src/__tests__/fetch.js
@@ -0,0 +1,23 @@
+const refApi = 'http://www.dnd5eapi.co/api/classes/';
+
+const testCode = document.getElementById(`testCode`);
+const addCode = document.getElementsByClassName('addCode');
+const testApiScript = document.getElementById(`DndApi`);
+
+let testHello = 'testHelloWorld';
+
+
+function fetchingApi () {
+    fetch(refApi)
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(data){
+            for(let i=0; i<data.results.length; i++) {
+                testApiScript.textContent += data.results[i].name;
+            }
+        
+        })
+};
+
+fetchingApi();
--- client/src/__tests__/fetchApi.test.js
+++ client/src/__tests__/fetchApi.test.js
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
 const refApi = 'http://www.dnd5eapi.co/swagger/openapi.json';
 
+let testHello = 'testHelloWorld'
+
 test('fetch API', () => {
   fetch(refApi)
     .then(function(res){
       return res.json();
     })
-})
+});
+
+
+testHello.append(document.getElementById(`testCode`));
